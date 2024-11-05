caf72e9d9722f6ca8e993e9886d17fa5863d7112
jinsu_park <jinsu_park@tmax.co.kr>
2024-11-05 10:15:50 +0900

    feat: useInput hook 추가

---
+++ src/custom-hooks/useInput.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useInput = (initialValue: string | number | boolean, validator: Function) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+            target: { value },
+        } = event;
+
+        let willUpdate = true;
+
+        if (typeof validator === "function") {
+            willUpdate = validator(value);
+        }
+        if (willUpdate) {
+            setValue(value);
+        }
+    };
+    return { value, onChange };
+};
